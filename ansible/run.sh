c0547f8df39c4af43162903e752f8ce21a3f3b1b
Victor Caldas <caldas.victor@gmail.com>
2026-02-13 09:02:00 +0100

    Ignore secrets from source control

---
+++ ansible/run.sh
@@ -0,0 +1,20 @@
+
+#!/bin/bash
+source ./vars.sh
+
+# Ensure SSH key exists
+if [ ! -f "$HOME/.ssh/id_ed25519" ]; then
+    echo "Error: SSH key not found at $HOME/.ssh/id_ed25519"
+    exit 1
+fi
+chmod 0700 $HOME/.ssh/id_ed25519
+
+# Create Ansible vault password file
+echo -n "$ANSIBLE_VAULT_PW" | tr -d '\r' > $HOME/vault-pw
+
+# Start ssh-agent and add key
+eval `ssh-agent -s`
+ssh-add $HOME/.ssh/id_ed25519
+
+# Run Ansible playbook
+ansible-playbook -v ./playbooks/$PLAYBOOK.yaml --inventory $INVENTORY --vault-password-file $HOME/vault-pw --ssh-extra-args "-o StrictHostKeyChecking=no" 
